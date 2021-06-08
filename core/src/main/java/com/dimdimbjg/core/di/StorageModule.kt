f14193d04e7a0f01a2d7f7c0a466c87ad2a41b03
Dimas Yuda Wahyu Buana <dimas.bjg@gmail.com>
2021-06-08 08:59:13 +0700

    add encryption on database.

--- core/src/main/java/com/dimdimbjg/core/di/StorageModule.kt
+++ core/src/main/java/com/dimdimbjg/core/di/StorageModule.kt
@@ -2,15 +2,21 @@ package com.dimdimbjg.core.di
 
 import androidx.room.Room
 import com.dimdimbjg.core.data.source.local.room.GamesDatabase
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val databaseModule = module {
     factory { get<GamesDatabase>().gamesDao() }
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("gamescatalogue".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
             GamesDatabase::class.java,"games.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
     }
 }
