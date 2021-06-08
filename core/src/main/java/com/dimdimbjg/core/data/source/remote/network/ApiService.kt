480f1ea294974dc491c404664f104577e1a12d69
Dimas Yuda Wahyu Buana <dimas.bjg@gmail.com>
2021-06-08 11:47:47 +0700

    add certificate pinning for connection to the server

--- core/src/main/java/com/dimdimbjg/core/data/source/remote/network/ApiService.kt
+++ core/src/main/java/com/dimdimbjg/core/data/source/remote/network/ApiService.kt
@@ -1,5 +1,6 @@
 package com.dimdimbjg.core.data.source.remote.network
 
+import com.dimdimbjg.core.BuildConfig
 import com.dimdimbjg.core.data.source.remote.response.DetailGameResponse
 import com.dimdimbjg.core.data.source.remote.response.ListGamesResponse
 import retrofit2.http.GET
@@ -8,7 +9,7 @@ import retrofit2.http.Path
 interface ApiService {
 
     companion object {
-        private const val api_key = "6d9b0a98094f420bb20a15ecd77cd595"
+        private const val api_key = BuildConfig.API_KEY
     }
 
     @GET("games?key=$api_key&platform=4&exclude_additions=true&exclude_parents=true")
--- core/src/main/java/com/dimdimbjg/core/di/NetworkModule.kt
+++ core/src/main/java/com/dimdimbjg/core/di/NetworkModule.kt
@@ -1,6 +1,7 @@
 package com.dimdimbjg.core.di
 
 import com.dimdimbjg.core.data.source.remote.network.ApiService
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
@@ -10,10 +11,16 @@ import java.util.concurrent.TimeUnit
 
 val networkModule = module {
     single {
+        val hostname = "api.rawg.io"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/R+V29DqDnO269dFhAAB5jMlZHepWpDGuoejXJjprh7A=")
+            .add(hostname, "sha256/FEzVOUp4dF3gI0ZVPRJhFbSJVXR+uQmMH65xhs1glH4=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
